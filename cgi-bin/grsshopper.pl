d63f518f9c11b0f9c9fde2bd055d05a202a9dd38
Stephen Downes <stephen@downes.ca>
2020-09-16 14:46:29 -0400

    Update grsshopper.pl

--- cgi-bin/grsshopper.pl
+++ cgi-bin/grsshopper.pl
@@ -9828,6 +9828,8 @@ sub check_user {
 
     if ($cgi->param("action") eq "logout") {
         $session->delete();
+	&show_login();
+	exit;
         #print $cgi->header();
         #print qq|Logged Out. <a href="//|.$ENV{'SERVER_NAME'}.$ENV{'SCRIPT_NAME'}.qq|">Login</a>|;
         #exit;

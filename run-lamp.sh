a189c941dbf464d6b7d8c100c6a5a5a9749eeac2
Stephen Downes <stephen@downes.ca>
2021-01-19 10:46:32 -0500

    Trying to fix ssl

--- run-lamp.sh
+++ run-lamp.sh
@@ -54,7 +54,7 @@ fi
 /bin/sed -i "s/\;date\.timezone\ \=/date\.timezone\ \=\ ${DATE_TIMEZONE}/" /etc/php/7.0/apache2/php.ini
 
 # Run Postfix
-/usr/sbin/postfix start
+# /usr/sbin/postfix start
 
 # Run MariaDB
 /usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&
